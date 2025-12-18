6a646864df8a053e997347b0391c19f91a5c19a1
Borja <lopzdiazborja@gmail.com>
2025-12-18 13:39:33 +0100

    remove unused React

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { SetupScreen } from "./screens/SetupScreen";
 import { GameScreen } from "./screens/GameScreen";
 
